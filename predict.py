12247d9e53a6498477eeb217ac446be56e3aa360
Younsi wassim <120186938+wassimMin@users.noreply.github.com>
2024-07-01 21:42:56 +0100

    ww

---
+++ predict.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,r2_score
+
+
+data = pd.read_csv('dataset/Housing.csv')
+
+data.head()
